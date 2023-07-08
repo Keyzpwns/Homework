93be196ea7a649918f51ab08e2fa4693d53f0c6b
Keyz <keyshawn.rollins@gmail.com>
2023-07-08 16:57:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 var age = 27; console.log(age);
 var name = "keyz"; console.log(name);
+var people = ["mindchael" , "kim" , "michelle" , "Mother"];
+for(var i=3; i<people.length; i--){
+console.log(people[i]);}
+const bankaccount = 2;
+const corona = 3;
+const margarita = 7;
+
+if(bankaccount>=margarita){
+    console.log('I can afford a margarita');
+  }  
+  else if (bankaccount >= corona) {
+        console.log('I can afford a corona');
+    }
+    else{
+        console.log('I cannot afford either margarita or corrona');
+    }
